9a1eed7cf10b2f7a705215dd7f5f16adad617fc6
Vvitali <vvitali26@gmail.com>
2018-05-08 21:12:15 -0400

    fixed navbar image - moved from nav to another place, react does not cry anymore

---
+++ src/components/generalFundVisual/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { geoMercator, geoPath } from 'd3-geo';
+import GeneralData from "../../data/FY2016_Budget_General_Fund.csv";	
+class WorldMap extends Component {
+	render() {
+		{console.log("run!")}
+		return <svg width={500} height={500}>
+
+		</svg>
+	}
+}
+export default WorldMap
