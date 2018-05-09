b9a5d44076332fa16c3f4f4ad2a0d4d911f86ac6
Vvitali <vvitali26@gmail.com>
2018-05-08 21:17:30 -0400

    axios, d3 installed, preparation for data entry from the file

--- src/components/generalFundVisual/index.js
+++ src/components/generalFundVisual/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { geoMercator, geoPath } from 'd3-geo';
-import GeneralData from "../../data/FY2016_Budget_General_Fund.csv";	
+import Axios from "axios";
+import GeneralData from "../../data/FY2016_Budget_General_Fund.csv";
+
 class WorldMap extends Component {
 	render() {
-		{console.log("run!")}
+		console.log(GeneralData);
 		return <svg width={500} height={500}>
-
 		</svg>
 	}
 }
-export default WorldMap
+
+export default WorldMap;
