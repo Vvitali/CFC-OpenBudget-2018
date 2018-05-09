bec98446bba0cc88b2f11b1347a7acb338941f41
Vvitali <vvitali26@gmail.com>
2018-05-08 21:20:00 -0400

    readme updated

--- src/routes/visualization/index.js
+++ src/routes/visualization/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 
+import Map from '../../components/generalFundVisual';
+
 const Visual = (prop)=>{
 	return(
-
 		<div>
 		<h1>Visualization placeholder!</h1>
+		<Map/>
 		</div>
 		);
 };
