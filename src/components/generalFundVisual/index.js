8476f291cc06dc99086b65dfbe30e8aa00cd7cd9
Vvitali <vvitali26@gmail.com>
2018-05-08 21:44:16 -0400

    util and index

--- src/components/generalFundVisual/index.js
+++ src/components/generalFundVisual/index.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import { geoMercator, geoPath } from 'd3-geo';
-import Axios from "axios";
-import GeneralData from "../../data/FY2016_Budget_General_Fund.csv";
-
+import axios from "axios"; 
+import csvParser from "../../util/csvParser.js"; 
 class WorldMap extends Component {
 	render() {
-		console.log(GeneralData);
+		axios.get('/data/FY2016_Budget_General_Fund.csv')
+		.then(function (response) {
+			console.log(response);
+			csvParser(response.data);
+		})
+		.catch(function (error) {
+			console.log(error);
+		});
+
 		return <svg width={500} height={500}>
 		</svg>
 	}
